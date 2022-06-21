f14a07e7eacc59603cb671716e8328c63cc93a6d
Nikhil Srivastava <53013690+Nikhil-22@users.noreply.github.com>
2022-06-21 22:45:49 +0530

    Deelete_Value

---
+++ DeleteValue.py
@@ -0,0 +1,12 @@
+numbers = int(input("Enter Number to Store: "))
+arr = []
+
+for i in range(numbers):
+    arr.append(int(input()))
+print(arr, end=" ")
+print()
+
+x = int(input("Enter Position of an Element you want to Delete: "))
+
+arr.pop(x)
+print(arr, end=" ")
